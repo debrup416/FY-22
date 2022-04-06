11f3e51980e5de0a3dee0397accf23c398f2c96e
debrup416 <debrup416@gmail.com>
2022-04-06 19:27:38 +0530

    Setup Done

---
+++ manage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+import pathlib
+import dotenv
+
+
+def main():
+    """Run administrative tasks."""
+    DOT_ENV_PATH=pathlib.Path() / '.env'
+    if DOT_ENV_PATH.exists():
+        dotenv.read_dotenv()
+    else:
+        print(".ENV doesn't exist")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogForum.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
