dc7e5c7d6195caf5d1070382f3bab0c02008dacc
debrup416 <debrup416@gmail.com>
2022-04-24 15:41:19 +0530

    Done

--- forum/urls.py
+++ forum/urls.py
@@ -4,11 +4,16 @@ from . import views
 app_name = 'forum'
 urlpatterns=[
     path('',views.home,name='home'),
+    path('tags',views.tags,name='tags'),
+    path('tag/<str:tag>/',views.tag,name='tag'),
     path('ask-question',views.ask_form,name='ask-question'),
     path('save-comment',views.save_comment,name='save-comment'),
     path('<slug:slug>',views.detail,name='detail'),
+    path('<slug:slug>/edit/', views.post_edit, name='ques_edit'),
+    path('<slug:slug>/del/',views.deletePost, name='ques_del'),
     path('<int:id>/<int:pk>/save-upvote',views.save_upvote,name='save-upvote'),
     path('<int:id>/<int:pk>/save-downvote',views.save_downvote,name='save-downvote'),
-    path('tag/<str:tag>',views.tag,name='tag'),
-    path('tags',views.tags,name='tags'),
+    
+    
+    
 ]
