ad27c7270dab73a8afd3a0f41bd82d7c15a2c417
debrup416 <debrup416@gmail.com>
2022-05-23 21:40:28 +0530

    done

--- static/js/blog.js
+++
@@ -1,7 +0,0 @@
-function cus_open() {
-    document.getElementById("id-sidebar").style.display = "block";
-}
-
-function cus_close() {
-    document.getElementById("id-sidebar").style.display = "none";
-}
