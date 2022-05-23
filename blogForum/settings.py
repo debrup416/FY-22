e53e1768fc93ec708851d1743c3db3d25b3174aa
debrup416 <debrup416@gmail.com>
2022-05-23 15:06:09 +0530

    2

--- blogForum/settings.py
+++ blogForum/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['http://127.0.0.1:8000/','https://grow2gether.herokuapp.com/','grow2gether.herokuapp.com']
 
 
 # Application definition
