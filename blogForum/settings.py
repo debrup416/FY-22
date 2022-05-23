b7839e12ecb86c538c40cb1524da2f1867b19307
debrup416 <debrup416@gmail.com>
2022-05-23 21:14:21 +0530

    ok

--- blogForum/settings.py
+++ blogForum/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000/','https://grow2gether.herokuapp.com/','grow2gether.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1' ,'http://127.0.0.1:8000/','https://grow2gether.herokuapp.com/','grow2gether.herokuapp.com']
 
 
 # Application definition
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -196,8 +197,12 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL=None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-django_heroku.settings(locals())
-
+# django_heroku.settings(locals())
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # try disabling it
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_MANIFEST_STRICT = False
+WHITENOISE_ALLOW_ALL_ORIGINS = True
 
 LOGGING = {
     'version': 1,
