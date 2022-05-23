85632b67a39715d568437324ed7c03e8955649b8
debrup416 <debrup416@gmail.com>
2022-05-23 20:53:54 +0530

    F

--- blogForum/settings.py
+++ blogForum/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['http://127.0.0.1:8000/','https://grow2gether.herokuapp.com/','grow2gether.herokuapp.com']
 
